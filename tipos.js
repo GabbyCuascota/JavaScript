cf9f2c593c2babdb25894ece285f837b5353f00f
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-17 15:07:43 -0500

    2

--- tipos.js
+++ tipos.js
@@ -1,6 +1,17 @@
 var nombre = "26"
 var edad = 26
 var fecha = new Date();
+var casado = false; //true
+var casa = undefined;
+var arreglo = []
+console.log(casa ? "Verdadero" : "Falso");
+if(casa){
+    console.log("Verdadero");
+}else{
+    console.log("Falso");
+}
+
+var carro = null;
 
 if (nombre !== edad){
     console.log("Verdadero")
