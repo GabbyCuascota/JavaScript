007bb73b6110b7b768cebbf98801ba3b8b178f55
graciela <chelinesmoreno@hotmail.com>
2018-04-20 16:47:18 -0500

    2

--- 01-tipos-variables.ts
+++ 01-tipos-variables.ts
@@ -1,1 +1,11 @@
 //MI REPOSITORIO
+//var nombre = "Gabby";
+//let segundoNombre: String ; //buena practica
+//let segundoNombre: string = 'Estefanía'; //mala practica por redundancia
+//let segundoNombre: any = 'Estefanía'; //variables antiguas para cualquier cosa
+let segundoNombre = 'Estefanía';
+const cedula = '1720437670';
+segundoNombre = 'Gabriela';
+//cedula = '1231231233'//da error de read-only
+const URL_POLI = 'http://www.epn.ed.ec';
+//segundoNombre = 123; //Duck Typing
