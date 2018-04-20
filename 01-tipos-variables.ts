5b4e29682048091b54c08bd19a2e62e1fe9c9838
graciela <chelinesmoreno@hotmail.com>
2018-04-20 17:08:47 -0500

    2

--- 01-tipos-variables.ts
+++ 01-tipos-variables.ts
@@ -9,3 +9,36 @@ segundoNombre = 'Gabriela';
 //cedula = '1231231233'//da error de read-only
 const URL_POLI = 'http://www.epn.ed.ec';
 //segundoNombre = 123; //Duck Typing
+
+
+//TIPOS DE DATOS
+let nombre: String;
+let edad: Number = 26;
+let fecha: Date = new Date();
+let casado: boolean = false;
+//let mascotas: object = {};
+
+//CREAR NUEVO TIPO DE DATO
+class Usuario {
+
+    public mNombre: String;
+    private mEdad: Number;
+    protected mCedula: String;
+
+    constructor(nombre: String,
+                edad: Number,
+                cedula: String,
+    ) {
+        this.mNombre = nombre;
+        this.mEdad = edad;
+        this.mCedula = cedula;
+    }
+
+    public imprimirEnConsola() {
+        //Template Strings TILDE INVERTIDA
+        console.log(`El usuario se llama ${this.mNombre}, 
+        la cedula del usuario es: ${this.mCedula}, 
+        la edad del usuario es: ${this.mEdad}`);
+    }
+}
+let usuario:Usuario = new Usuario ('Gabriela',26, '1720437670');
