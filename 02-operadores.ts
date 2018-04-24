1a5c49cf99c0a06cfee528db0332051c3e67a952
graciela <chelinesmoreno@hotmail.com>
2018-04-24 15:18:20 -0500

    4

--- 02-operadores.ts
+++ 02-operadores.ts
@@ -1,20 +1,34 @@
 //MI REPOSITORIO
 
 let arregloNumeros = [1, 2, 3, 4, 5];
-let sumarDosNumeros = (numeroUno: number, numeroDos: number) => { //funcion de flecha gorda se cambia la palabra function por =>
+//funcion de flecha gorda se cambia la palabra function por =>
+/*let sumarDosNumeros = (numeroUno : number, numeroDos : number) => {
     return numeroUno + numeroDos;
-};
-let totalEdades = a => ['Facultad1', 'Facultad2']; //primera forma
+};*/
+let facultadesDos = (a : number, b : number) => ['Facultad1', 'Facultad2']; //primera forma
 
-/*let totalEdades = function(){       //segunda forma
+
+/*let facultadesUno = function(){       //segunda forma
     return  ['Facultad1', 'Facultad2'];
 }*/
 
-
-arregloNumeros.forEach(
+//ooperador
+/*arregloNumeros.forEach(
     (valor, indice, arreglo) => {
         console.log('valor', valor);
         console.log('indice', indice);
         console.log('arreglo', arreglo);
     }
+);*/
+let sumar = 0;
+let resultadoForEach= arregloNumeros.forEach(
+    (valor, indice, arreglo) => {
+        sumar = sumar + valor;
+        console.log(sumar);
+    });
+
+let resultadoDeLaSuma = arregloNumeros.reduce((totalAcumulado, valorArreglo)=> {
+return totalAcumulado + valorArreglo;
+},
+    0
 );
