5c73394144f007f85725f034caf02793f63b1455
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-27 16:15:08 -0500

    1

--- 02-operadores.ts
+++ 02-operadores.ts
@@ -1,16 +1,16 @@
 //MI REPOSITORIO
 
 let arregloNumeros = [1, 2, 3, 4, 5];
-let arregloDeudas = [78, 56, 180, 590, 64, 78];
+let arregloDeudas = [73, 123.32, 23, 43, 123, 43, 50, 1];
 
-//let arregloUsuario: Array<UsuarioArreglo> = [
-let arregloUsuario: UsuarioArreglo[] = [
+// let arregloUsuarios: Array<UsuarioArreglo> =[
+let arregloUsuarios : UsuarioArreglo[] = [
     {
         nombre: 'Gabriela',
         edad: 28
     },
     {
-        nombre: 'Carla',
+        nombre: 'Estefanía',
         edad: 10
     },
     {
@@ -18,102 +18,95 @@ let arregloUsuario: UsuarioArreglo[] = [
         edad: 70
     },
     {
-        nombre: 'Ivan',
+        nombre: 'Adrian',
         edad: 32
     },
     {
-        nombre: 'Miguel',
-        edad: 25}];
+        nombre: 'Paulina',
+        edad: 25
+    },
+];
 
-//funcion de flecha gorda se cambia la palabra function por =>
-let sumarDosNumeros = (numeroUno: number, numeroDos: number) => {
+let sumarDosNumeros = (numeroUno: number, numeroDos: number) => { //fat arrow funct}
     return numeroUno + numeroDos;
 };
 
 let totalEdades = arregloDeNumeros => {
     // codigo extra
 };
-
 let facultades = function (a) {
     return ['Facultad1', 'Facultad2'];
 };
+let facultadesDos = (a: number, b: number) => ['Facultad1', 'Facultad2'];
 
-let facultadesDos = (a: number, b: number) => ['Facultad1', 'Facultad2']; //primera forma
+
 facultadesDos(1, 2);
 
-let facultadesUno = function(){       //segunda forma
-    return  ['Facultad1', 'Facultad2'];
 
-
-//ooperador
-/*arregloNumeros.forEach(
-    (valor, indice, arreglo) => {
-        console.log('valor', valor);
-        console.log('indice', indice);
-        console.log('arreglo', arreglo);
-    }
-);*/
+// operador
 let sumar = 0;
-let resultadoForEach= arregloNumeros.forEach(
+let resultadoForEach = arregloNumeros.forEach(
     (valor, indice, arreglo) => {
         sumar = sumar + valor;
         console.log(sumar);
-    });
-
-let resultadoDeLaSuma = arregloNumeros.reduce((totalAcumulado, valorArreglo)=> {
-return totalAcumulado - valorArreglo;
-},
-    20
+    }
 );
 
-    let resultadoDeLaSumaDos = arregloNumeros
-        .reduce(
-            (totalAcumulado, valorArreglo) => {
-                return totalAcumulado - valorArreglo;
-            },
-            20
-        );
+let resultadoDeLaSuma = arregloNumeros
+    .reduce(
+        (totalAcumulado, valorArreglo) => {
+            return totalAcumulado - valorArreglo;
+        },
+        20
+    );
 
+let resultadoDeLaSumaDos = arregloNumeros
+    .reduce(
+        (totalAcumulado, valorArreglo) => {
+            return totalAcumulado - valorArreglo;
+        },
+        20
+    );
 console.log('resultadoDeLaSuma', resultadoDeLaSuma);
 
-    function calcularDeudaDeUsuario(edad: number) {
-        return arregloDeudas.reduce(
-            (totalAcumulado, deuda: number) => {
-                return totalAcumulado + (
-                    ((edad) / 100) * deuda
-                )
-            }, 0);
-    }
+
+function calcularDeudaDeUsuario(edad: number) {
+    return arregloDeudas.reduce(
+        (totalAcumulado, deuda: number) => {
+            return totalAcumulado + (
+                ((edad) / 100) * deuda
+            )
+        }, 0);
+}
 
 
-    let usuariosConCincoAniosMenos = arregloUsuario
-        .map(
-            (usuario: UsuarioArreglo) => {
-                usuario.edad = usuario.edad - 5;
-                usuario.deuda = calcularDeudaDeUsuario(usuario.edad);
-                return usuario;
-            }
-        )
-        .filter((usuario: UsuarioArreglo)=>{
-        return (usuario.deuda < 100);
-
-        });
-
-
-    console.log('usuariosConCincoAniosMenos', usuariosConCincoAniosMenos);
-
-    let resultadoDeLasEdades = arregloUsuario
-        .reduce(
-            (totalEdadAcumulado, usuarioArreglo: UsuarioArreglo) => {
-                return totalEdadAcumulado + usuarioArreglo.edad;
-            },
-            0
-        );
-    console.log('resultadoDeLasEdades', resultadoDeLasEdades);
+let usuariosConCincoAniosMenos = arregloUsuarios
+    .map(
+        (usuario: UsuarioArreglo) => {
+            usuario.edad = usuario.edad - 5;
+            usuario.deuda = calcularDeudaDeUsuario(usuario.edad);
+            return usuario;
+        }
+    )
+    .filter((usuario:UsuarioArreglo)=>{
+        // true se devuelve, false no se devuelve
+        return (usuario.deuda<100);
+    });
 
 
+console.log('usuariosConCincoAniosMenos', usuariosConCincoAniosMenos);
 
-    interface UsuarioArreglo{
+let resultadoDeLasEdades = arregloUsuarios
+    .reduce(
+        (totalEdadAcumulado, usuarioArreglo: UsuarioArreglo) => {
+            return totalEdadAcumulado + usuarioArreglo.edad;
+        },
+        0
+    );
+console.log('resultadoDeLasEdades', resultadoDeLasEdades);
+
+
+interface UsuarioArreglo {
     nombre: string;
     edad: number;
     deuda?: number;
